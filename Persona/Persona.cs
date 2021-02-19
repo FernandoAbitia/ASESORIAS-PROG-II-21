a3cfc735706235505f0d0f3a262d8921287c267f
Fernando <fernando_abitia@hotmail.com>
2021-02-19 12:32:02 -0700

    Personas
    Ejercicio inicial (clase base, estructura, ejemplo)

---
+++ Persona/Persona.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    class Persona
+    {
+        private int Edad;
+        private string Nombre;
+
+        public Persona(int Edad, string Nombre)
+        {
+            this.Edad = Edad;
+            this.Nombre = Nombre;
+        }
+
+        public int pEdad
+        {
+            get
+            {
+                return Edad;
+            }
+
+            set
+            {
+                Edad = value;
+            }
+
+        }
+
+        public string pNombre
+        {
+            get
+            {
+                return Nombre;
+            }
+
+            set
+            {
+                Nombre = value;
+            }
+        }
+    }
+}
---
+++ Persona/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    class Program
+    {
+        //DEFINIR ARREGLO DE PERSONAS
+        public Program()
+        {
+            //INICIALIZAR
+            Principal();
+        }
+
+        public void Principal()
+        {
+            int Edad;
+            string Nombre;
+
+            Console.WriteLine("ESCRIBE TU NOMBRE");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("ESCRIBE TU EDAD");
+            Edad = Convert.ToInt32(Console.ReadLine());
+
+            Persona A = new Persona(Edad,Nombre);
+
+            Console.WriteLine("SU NOMBRE ES: "+A.pNombre);
+            Console.WriteLine("SU EDAD ES: "+A.pEdad);
+
+            Console.ReadKey();
+
+            Console.WriteLine("CUÁL ES SU NUEVO NOMBRE");
+            Nombre = Console.ReadLine();
+
+            A.pNombre = Nombre;
+
+            Console.WriteLine("SU NUEVO NOMBRE ES: "+A.pNombre);
+
+            Console.ReadKey();
+        }
+
+
+        static void Main(string[] args)
+        {
+           new Program();
+        }
+    }
+}
